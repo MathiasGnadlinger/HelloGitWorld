e6690683a687ed40f5b58cf2390c02be1632d892
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-10-08 13:09:24 +0200

    Rechnung mit Eingabe

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,23 @@ public class Main
         try
         {
             Scanner sc = new Scanner(System.in);
-            System.out.printf("Geben Sie einen Wert ein: ");
+            System.out.printf("Geben Sie einen Wert ein: %n");
             int i = sc.nextInt();
+
+            System.out.printf("Geben Sie den zweiten Wert ein, den Sie addieren möchten: ");
+            int o = sc.nextInt();
+
+            int sum = i +o;
+
+            System.out.printf("Das Ergebnis bertägt: %d",sum);;
+
         }
         catch(InputMismatchException ex)
         {
             System.out.printf("Es wurde kein nummerischer Wert eingegeben!!");
         }
+
+
+
     }
 }
