d4ec89a586a9729d37689030db88a38526909932
Mathias Gnadlinger <mathias.gnadlinger@htl-wels.at>
2020-10-08 12:47:10 +0200

    Programm Erstellung

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Mathias Gnadlinger
+ * @version 4, 08.10.2020
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Hello World");
+    }
+}
